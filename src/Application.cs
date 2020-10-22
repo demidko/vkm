ad40f525aa510cd13306e54c7c7a9b2013bed8dd
Daniel Demidko <demidko@farpost.com>
2020-10-23 05:27:48 +1000

    Микрорефакторинг

--- src/Application.cs
+++ src/Application.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Immutable;
-using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using VkNet.Model.RequestParams;
+using static System.ConsoleColor;
+using static System.IO.File;
 using static System.Text.RegularExpressions.Regex;
 
 
@@ -16,31 +18,27 @@ internal static class Application
 
     private static async Task Main(string[] args)
     {
-        var lemma = args
-            .LastOrDefault()
-            ?.ToLowerInvariant() ?? throw new ArgumentException(UsageGuide);
-        
-        var audios = args.SkipLast(1)
-            .ToImmutableList()
+        var lemma = args.LastOrDefault()?.ToUpperInvariant()
+                    ?? throw new ArgumentException(UsageGuide);
+
+        var audios = args.SkipLast(1).ToImmutableList()
             .LoginForVkApi()
-            .Audio
-            .Get(new AudioGetParams {Count = 6000})
-            .Where(audio => audio.Title.ToLowerInvariant().Contains(lemma))
-            .Select(audio => (Filename: $"{audio.Title}.mp3", Url: audio.Url.RestoreMp3Url()));
-        
+            .Audio.Get(new AudioGetParams {Count = 6000})
+            .Where(x => x.Title.ToUpperInvariant().Contains(lemma))
+            .Select(x => (Filename: $"{x.Title}.mp3", Url: x.Url.RestoreMp3()));
+
         using var downloader = new HttpClient();
 
         foreach (var (filename, url) in audios)
         {
-            $"Downloading {filename}".Println(ConsoleColor.DarkBlue);
-            var bytes = await downloader.GetByteArrayAsync(url);
-            await File.WriteAllBytesAsync(filename, bytes);
+            $"Downloading {filename}".Println(DarkBlue);
+            await WriteAllBytesAsync(filename, await downloader.GetByteArrayAsync(url));
         }
     }
 
-    private static Uri RestoreMp3Url(this Uri url) => new Uri(Replace(
+    private static string RestoreMp3(this Uri url) => Regex.Replace(
         url.ToString(),
         @"/[a-zA-Z\d]{6,}(/.*?[a-zA-Z\d]+?)/index.m3u8()",
         @"$1$2.mp3"
-    ));
+    );
 }
