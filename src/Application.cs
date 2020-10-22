0e700192fa6b37413b9aabae71cc4db9c06118cb
Daniel Demidko <demidko@farpost.com>
2020-10-23 05:31:10 +1000

    Микрорефакторинг

--- src/Application.cs
+++ src/Application.cs
@@ -12,33 +12,32 @@ using static System.Text.RegularExpressions.Regex;
 
 internal static class Application
 {
-    internal const string UsageGuide = "Invalid arguments. Usage:\n" +
-                                       "  With authorization data: dotnet vkm [login] [password] [audio]\n" +
-                                       "  With cache: dotnet vkm [audio]";
+  internal const string Guide = "Invalid arguments. Usage:\n" +
+                                "  With authorization data: dotnet vkm [login] [password] [audio]\n" +
+                                "  With cache: dotnet vkm [audio]";
 
-    private static async Task Main(string[] args)
+  private static async Task Main(string[] args)
+  {
+    var lemma = args.LastOrDefault()?.ToUpperInvariant() ?? throw new ArgumentException(Guide);
+
+    var audios = args.SkipLast(1).ToImmutableList()
+      .LoginForVkApi()
+      .Audio.Get(new AudioGetParams {Count = 6000})
+      .Where(x => x.Title.ToUpperInvariant().Contains(lemma))
+      .Select(x => (Filename: $"{x.Title}.mp3", Url: x.Url.RestoreMp3()));
+
+    using var downloader = new HttpClient();
+
+    foreach (var (filename, url) in audios)
     {
-        var lemma = args.LastOrDefault()?.ToUpperInvariant()
-                    ?? throw new ArgumentException(UsageGuide);
-
-        var audios = args.SkipLast(1).ToImmutableList()
-            .LoginForVkApi()
-            .Audio.Get(new AudioGetParams {Count = 6000})
-            .Where(x => x.Title.ToUpperInvariant().Contains(lemma))
-            .Select(x => (Filename: $"{x.Title}.mp3", Url: x.Url.RestoreMp3()));
-
-        using var downloader = new HttpClient();
-
-        foreach (var (filename, url) in audios)
-        {
-            $"Downloading {filename}".Println(DarkBlue);
-            await WriteAllBytesAsync(filename, await downloader.GetByteArrayAsync(url));
-        }
+      $"Downloading {filename}".Println(DarkBlue);
+      await WriteAllBytesAsync(filename, await downloader.GetByteArrayAsync(url));
     }
+  }
 
-    private static string RestoreMp3(this Uri url) => Regex.Replace(
-        url.ToString(),
-        @"/[a-zA-Z\d]{6,}(/.*?[a-zA-Z\d]+?)/index.m3u8()",
-        @"$1$2.mp3"
-    );
+  private static string RestoreMp3(this Uri url) => Regex.Replace(
+    url.ToString(),
+    @"/[a-zA-Z\d]{6,}(/.*?[a-zA-Z\d]+?)/index.m3u8()",
+    @"$1$2.mp3"
+  );
 }
--- src/Vk.cs
+++ src/Vk.cs
@@ -18,73 +18,73 @@ using static VkNet.Enums.Filters.Settings;
 /// </summary>
 internal static class Vk
 {
-    /// <summary>
-    /// Кеш содержит две строки (1 логин, 2 пароль)
-    /// </summary>
-    private const string CachePath = ".authorization";
+  /// <summary>
+  /// Кеш содержит две строки (1 логин, 2 пароль)
+  /// </summary>
+  private const string CachePath = ".authorization";
 
-    /// <summary>
-    /// Метод входит под именем и паролем пользователя
-    /// </summary>
-    /// <param name="args">логин + пароль или пустой список</param>
-    /// <returns>VK Api</returns>
-    internal static VkApi LoginForVkApi(this IReadOnlyList<string> args)
+  /// <summary>
+  /// Метод входит под именем и паролем пользователя
+  /// </summary>
+  /// <param name="args">логин + пароль или пустой список</param>
+  /// <returns>VK Api</returns>
+  internal static VkApi LoginForVkApi(this IReadOnlyList<string> args)
+  {
+    var services = new ServiceCollection();
+    // Включаем доступ к своим сообщениям и комментариям
+    services.AddAudioBypass();
+    var api = new VkApi(services);
+    var (login, password) = LoadAuthorizationData(args);
+    api.Authorize(new ApiAuthParams
     {
-        var services = new ServiceCollection();
-        // Включаем доступ к своим сообщениям и комментариям
-        services.AddAudioBypass();
-        var api = new VkApi(services);
-        var (login, password) = LoadAuthorizationData(args);
-        api.Authorize(new ApiAuthParams
-        {
-            // Используем идентификатор который откопали где-то в интернете
-            ApplicationId = 1980660,
-            Login = login,
-            Password = password,
-            Settings = All
-        });
-        $"Login as vk.com/id{api.UserId}".Println(DarkBlue);
-        return api;
-    }
+      // Используем идентификатор который откопали где-то в интернете
+      ApplicationId = 1980660,
+      Login = login,
+      Password = password,
+      Settings = All
+    });
+    $"Login as vk.com/id{api.UserId}".Println(DarkBlue);
+    return api;
+  }
 
-    /// <summary>
-    /// Метод загружает пару (логин, пароль) с помощью аргументов
-    /// </summary>
-    private static (string Login, string Password) LoadAuthorizationData(IReadOnlyList<string> args) =>
-        args.Count switch
-        {
-            0 => LoadAuthorizationDataFromCache(),
-            2 => LoadAuthorizationDataFromInput(args),
-            _ => throw new ArgumentException(UsageGuide)
-        };
-
-
-    /// <summary>
-    /// Метод извлекает пару (логин, пароль) из аргументов
-    /// </summary>
-    private static (string Login, string Password) LoadAuthorizationDataFromInput(IReadOnlyList<string> args)
+  /// <summary>
+  /// Метод загружает пару (логин, пароль) с помощью аргументов
+  /// </summary>
+  private static (string Login, string Password) LoadAuthorizationData(IReadOnlyList<string> args) =>
+    args.Count switch
     {
-        WriteAllLines(CachePath, args);
-        return (args[0], args[1]);
-    }
+      0 => LoadAuthorizationDataFromCache(),
+      2 => LoadAuthorizationDataFromInput(args),
+      _ => throw new ArgumentException(Guide)
+    };
 
-    /// <summary>
-    /// Метод извлекает пару (логин, пароль) из кеша
-    /// </summary>
-    private static (string Login, string Password) LoadAuthorizationDataFromCache()
+
+  /// <summary>
+  /// Метод извлекает пару (логин, пароль) из аргументов
+  /// </summary>
+  private static (string Login, string Password) LoadAuthorizationDataFromInput(IReadOnlyList<string> args)
+  {
+    WriteAllLines(CachePath, args);
+    return (args[0], args[1]);
+  }
+
+  /// <summary>
+  /// Метод извлекает пару (логин, пароль) из кеша
+  /// </summary>
+  private static (string Login, string Password) LoadAuthorizationDataFromCache()
+  {
+    "Reading login and password from cache...".Println(DarkBlue);
+    if (!Exists(CachePath))
     {
-        "Reading login and password from cache...".Println(DarkBlue);
-        if (!Exists(CachePath))
-        {
-            throw new FileNotFoundException(
-                $"Authorization cache wasn't found. Please restart application with login and password"
-            );
-        }
-        var lines = ReadAllLines(CachePath);
-        return lines.Length == 2
-            ? (lines[0], lines[1])
-            : throw new IOException(
-                $"Invalid authorization cache. Please restart application with login and password"
-            );
+      throw new FileNotFoundException(
+        $"Authorization cache wasn't found. Please restart application with login and password"
+      );
     }
+    var lines = ReadAllLines(CachePath);
+    return lines.Length == 2
+      ? (lines[0], lines[1])
+      : throw new IOException(
+        $"Invalid authorization cache. Please restart application with login and password"
+      );
+  }
 }
