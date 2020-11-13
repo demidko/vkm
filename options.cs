49b22b4b4c60cad4dda085a6681fd9f6f1aedc25
Daniel Demidko <demidko@farpost.com>
2020-11-14 04:37:16 +1000

    Новые параметры

--- options.cs
+++ options.cs
@@ -7,17 +7,17 @@ internal class Options
         HelpText = "The name of your track. If the parameter is missing, all your music will be loaded.")]
     public string Title { init; get; } = Empty;
 
-    [Option('l', "title", Required = false,
+    [Option('l', "login", Required = false,
         HelpText =
             "Your phone number or email to enter VK. If the parameter is missing, the previous login will be used.")]
     public string Login { init; get; } = Empty;
 
-    [Option('l', "title", Required = false,
+    [Option('p', "password", Required = false,
         HelpText =
             "The VK password is not transferred anywhere from your computer. If the parameter is missing, the previous password will be used.")]
     public string Password { init; get; } = Empty;
 
-    [Option('p', "path", Required = false,
+    [Option('d', "directory", Required = false,
         HelpText =
             "Music download directory. If the directory does not exist, it will be created. The default is the current directory")]
     public string Path { init; get; } = Empty;
