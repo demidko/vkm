7952a0480dca8fe852f6b03ebc0642773d69427a
Daniel Demidko <demidko@farpost.com>
2020-11-14 01:57:55 +1000

    Переход на .NET 5

--- Application.cs
+++ Application.cs
@@ -10,10 +10,11 @@ using static System.ConsoleColor;
 using static System.IO.File;
 using static Vk;
 
+
 var vk = args.Length switch
 {
     1 => LoginToVkApi(),
-    3 => LoginToVkApi(args[0], args[1]),
+    2 or 3 => LoginToVkApi(args[0], args[1]),
     _ => throw new ArgumentException(
         "Invalid arguments. Usage:\n" +
         "  With authorization data: dotnet vkm [login] [password] [audio]\n" +
@@ -21,10 +22,13 @@ var vk = args.Length switch
     )
 };
 
-var lemma = args.Last().ToUpperInvariant();
 
-var audios = vk.Audio.Get(new AudioGetParams { Count = 6000 })
-    .Where(x => x.Title.ToUpperInvariant().Contains(lemma))
+Func<Audio, bool> filter = args.ElementAtOrDefault(3) is var lemma && lemma is null
+    ? x => true
+    : x => x.Title.ToUpperInvariant().Contains(lemma);
+
+var audios = vk.Audio.Get(new AudioGetParams {Count = 6000})
+    .Where(filter)
     .Select(x => (x.Title, Url: Regex.Replace(
         x.Url.ToString(),
         @"/[a-zA-Z\d]{6,}(/.*?[a-zA-Z\d]+?)/index.m3u8()",
